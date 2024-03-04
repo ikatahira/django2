07331feb8bd53f01d29c67ac8b22986eb6d44c6d
ikatahira <isaque.katahira@fatec.sp.gov.br>
2024-03-04 18:11:26 +0000

    aula 2

--- projetodjango/virtual/bin/sum_app/views.py
+++ projetodjango/virtual/bin/sum_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def sum_numbers(request, num1, num2):
+    result = int(num1) + int(num2)
+    return HttpResponse(f'A som a de {num1} e {num2} é {result}.')
+    
